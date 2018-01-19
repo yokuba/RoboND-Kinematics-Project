a89f609194d4205eae389a3c5c2b0be538c524ee
Alicia MacLennan <aflyingfemme@gmail.com>
2018-01-19 08:59:51 -0800

    Fix indentation

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -42,180 +42,107 @@ def handle_calculate_IK(req):
                         alpha5: -pi/2., a5: 0, d6: 0, q6: q6,
                         alpha6: 0, a6: 0, d7: 0.303, q7: 0}
 
-    # Define Modified DH Transformation matrix
-    # Helpful hint from the walkthrough (https://www.youtube.com/watch?v=Gt8DRm-REt4) to make this a function
-    def TF_Matrix(alpha, a, d, q):
-        TF = Matrix([[ cos(q), -sin(q), 0, a],
-            [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-            [ sin(q)*sin(alpha), cos(q)*sin(alpha), cos(alpha), cos(alpha)*d],
-            [ 0,            0,          0,  1]])
-        return TF
+            # Define Modified DH Transformation matrix
+            # Helpful hint from the walkthrough (https://www.youtube.com/watch?v=Gt8DRm-REt4) to make this a function
+            def TF_Matrix(alpha, a, d, q):
+                TF = Matrix([[cos(q), -sin(q), 0, a],
+                            [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                            [sin(q)*sin(alpha), cos(q)*sin(alpha), cos(alpha), cos(alpha)*d],
+                            [0,            0,          0,  1]])
+                return TF
 
-    # Create individual transformation matrices
-    T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(DH_Table)
-    T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(DH_Table)
-    T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(DH_Table)
-    T3_4 = TF_Matrix(alpha3, a3, d4, q4).subs(DH_Table)
-    T4_5 = TF_Matrix(alpha4, a4, d5, q5).subs(DH_Table)
-    T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(DH_Table)
-    T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(DH_Table)
+            # Create individual transformation matrices
+            T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(DH_Table)
+            T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(DH_Table)
+            T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(DH_Table)
+            T3_4 = TF_Matrix(alpha3, a3, d4, q4).subs(DH_Table)
+            T4_5 = TF_Matrix(alpha4, a4, d5, q5).subs(DH_Table)
+            T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(DH_Table)
+            T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(DH_Table)
 
-    # Incremental tranforms from base link to end effector
-    T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
+            # Incremental tranforms from base link to end effector
+            T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
 
-    # Extract the position and orientation point of end effector from request
-    # End effector position (px, py, pz)
-    # End effetor orientation is roll, pitch, yaw
-    px = req.poses[x].position.x
-    py = req.poses[x].position.y
-    pz = req.poses[x].position.z
+            # Extract the position and orientation point of end effector from request
+            # End effector position (px, py, pz)
+            # End effetor orientation is roll, pitch, yaw
+            px = req.poses[x].position.x
+            py = req.poses[x].position.y
+            pz = req.poses[x].position.z
 
-    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-        [req.poses[x].orientation.x, req.poses[x].orientation.y,
-            req.poses[x].orientation.z, req.poses[x].orientation.w])
+            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+                [req.poses[x].orientation.x, req.poses[x].orientation.y,
+                    req.poses[x].orientation.z, req.poses[x].orientation.w])
 
-    # EE rotation matrix
-    # reference http://planning.cs.uiuc.edu/node102.html
+            # EE rotation matrix
+            # reference http://planning.cs.uiuc.edu/node102.html
 
-    r, p, y = symbols('r p y')
+            r, p, y = symbols('r p y')
 
-    ROT_x = Matrix([[1, 0, 0],
-            [0, cos(r), -sin(r)],
-            [0, sin(r), cos(r)]]) #ROLL
+            ROT_x = Matrix([[1, 0, 0],
+                            [0, cos(r), -sin(r)],
+                            [0, sin(r), cos(r)]])  # ROLL
 
-    ROT_y = Matrix([[cos(p),   0, sin(p)],
-            [0,        1,  0],
-            [-sin(p),  0, cos(p)]]) #PITCH
+            ROT_y = Matrix([[cos(p),   0, sin(p)],
+                            [0,        1,  0],
+                            [-sin(p),  0, cos(p)]])  # PITCH
 
+            ROT_z = Matrix([[cos(y),   -sin(y), 0],
+                            [sin(y),    cos(y), 0],
+                            [0,         0,      1]])  # YAW
 
-    ROT_z = Matrix([[cos(y),   -sin(y), 0],
-            [sin(y),    cos(y), 0],
-            [0,         0,      1]])  #YAW
+            ROT_EE = ROT_z * ROT_y * ROT_x
 
-    ROT_EE = ROT_z * ROT_y * ROT_x
+            # aligning DH table with the URDF file (KR210 FK section)
+            Rot_Error = ROT_z.subs(y, radians(100)) * ROT_y.subs(p, radians(-90))
 
-    # aligning DH table with the URDF file (KR210 FK section)
-    Rot_Error = ROT_z.subs(y, radians(100)) * ROT_y.subs(p, radians(-90))
+            ROT_EE = ROT_EE * Rot_Error
+            ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
 
-    ROT_EE = ROT_EE * Rot_Error
-    ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
+            EE = Matrix([[px],
+                         [py],
+                         [pz]])
 
-    EE = Matrix([[px],
-                 [py],
-                 [pz]])
+            WC = EE - (0.303) * ROT_EE[:, 2]
 
-    WC = EE - (0.303) * ROT_EE[:,2]
+            # Joint angles (IK with Kuka KR210)
+            theta1 = atan2(WC[1], WC[2])
 
-    # Joint angles (IK with Kuka KR210)
-    theta1 = atan2(WC[1], WC[2])
+            #SSS triangle for theta2 and theta3
+            side_a = 1.501
+            side_b = sqrt(pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) + pow((WC[2] - 0.75),  2))
+            side_c = 1.25
 
-    #SSS triangle for theta2 and theta3
-    side_a = 1.501
-    side_b = sqrt(pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) + pow((WC[2] - 0.75),  2))
-    side_c = 1.25
+            angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
+            angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
+            angle_c = acos((side_b * side_b + side_a * side_a - side_c * side_c) / (2 * side_a * side_b))
 
-    angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
-    angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
-    angle_c = acos((side_b * side_b + side_a * side_a - side_c * side_c) / (2 * side_a * side_b))
+            theta2 = pi / 2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
+            theta3 = pi / 2 - (angle_b + 0.036)  # Sag in link4 of -0.054m
 
-    theta2 = pi / 2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
-    theta3 = pi / 2 - (angle_b + 0.036) #apparently the screen actor's guild is in link4 of -0.054m
+            R0_3 = T0_1[0:3, 0:3] * T1_2[0:3, 0:3] * T2_3[0:3, 0:3]
+            R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
 
-    R0_3 = T0_1[0:3, 0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
-    R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+            R3_6 = R0_3.inv("LU") * ROT_EE
 
-    R3_6 = R0_3.inv("LU") * ROT_EE
+            #Euler angles from rotation matrix
+            theta4 = atan2(R3_6[2, 2], -R3_6[0, 2])
+            theta5 = atan2(sqrt(R3_6[0, 2]*R3_6[0, 2] + R3_6[2, 2] * R3_6[2, 2]), R3_6[1, 2])
+            theta6 = atan2(-R3_6[1, 1], R3_6[1, 0])
 
-    #Euler angles from rotation matrix
-    theta4 = atan2(R3_6[2,2], -R3_6[0,2])
-    theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2] * R3_6[2,2]), R3_6[1,2])
-    theta6 = atan2(-R3_6[1,1], R3_6[1,0])
-
-
-    # optional FK
-    FK = T0_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
-# Initialize service response
-                joint_trajectory_list = []
-                for x in xrange(0, len(req.poses)):
+            # optional FK
+            FK = T0_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
+                # Initialize service response
+            joint_trajectory_list = []
+            for x in xrange(0, len(req.poses)):
                         # IK code starts here
                         joint_trajectory_point = JointTrajectoryPoint()
-
-            # Extract end-effector position and orientation from request
-            # px,py,pz = end-effector position
-            # roll, pitch, yaw = end-effector orientation
-                        px = req.poses[x].position.x
-                        py = req.poses[x].position.y
-                        pz = req.poses[x].position.z
-
-                        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-                                [req.poses[x].orientation.x, req.poses[x].orientation.y,
-                                        req.poses[x].orientation.z, req.poses[x].orientation.w])
-                          # EE rotation matrix
-    # reference http://planning.cs.uiuc.edu/node102.html
-
-    r, p, y = symbols('r p y')
-
-    ROT_x = Matrix([[1, 0, 0],
-    [0, cos(r), -sin(r)],
-    [0, sin(r), cos(r)]]) # ROLL about the x axis
-
-    ROT_y = Matrix([[cos(p),   0, sin(p)],
-    [0,        1,  0],
-    [-sin(p),  0, cos(p)]]) # PITCH about the y axis
-
-
-    ROT_z = Matrix([[cos(y),   -sin(y), 0],
-    [sin(y),    cos(y), 0],
-    [0,         0,      1]])  # YAW is rotation about the z axis
-
-    ROT_EE = ROT_z * ROT_y * ROT_x
-
-    # aligning DH table with the URDF file (KR210 FK section)
-    Rot_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
-
-    ROT_EE = ROT_EE * Rot_Error
-    ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
-
-    EE = Matrix([[px],
-    [py],
-    [pz]])
-
-    WC = EE - (0.303) * ROT_EE[:,2]
-
-    # Joint angles (IK with Kuka KR210)
-    theta1 = atan2(WC[1], WC[0])
-
-    #SSS triangle for theta2 and theta3
-    side_a = 1.501
-    side_b = sqrt(pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) + pow((WC[2] - 0.75),  2))
-    side_c = 1.25
-
-    angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
-    angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
-    angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
-
-    theta2 = pi / 2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
-    theta3 = pi / 2 - (angle_b + 0.036) #apparently the sag in link4 is -0.054m
-
-    R0_3 = T0_1[0:3, 0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
-    R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
-
-    R3_6 = R0_3.inv("LU") * ROT_EE
-
-    #Euler angles from rotation matrix
-    theta4 = atan2(R3_6[2,2], -R3_6[0,2])
-    theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2] * R3_6[2,2]), R3_6[1,2])
-    theta6 = atan2(-R3_6[1,1], R3_6[1,0])
-
-
-    # optional FK
-    FK = T0_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
 
             joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
             joint_trajectory_list.append(joint_trajectory_point)
 
-                rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
-                return CalculateIKResponse(joint_trajectory_list)
+            rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+            return CalculateIKResponse(joint_trajectory_list)
 
 
 def IK_server():
